fcba107cedaf3de5a67107a7df73e5f6c2da568d
SusanChoi <susan@akaspace.kr>
2017-02-11 16:13:23 +0900

    change dev config

--- android_overlay/device/fsl/imx6/imx6.mk
+++ android_overlay/device/fsl/imx6/imx6.mk
@@ -369,7 +369,7 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 # for property
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
-	persist.sys.usb.config=mtp,adb
+	persist.sys.usb.config=mtp
 
 PRODUCT_DEFAULT_DEV_CERTIFICATE := \
         device/fsl/common/security/testkey
