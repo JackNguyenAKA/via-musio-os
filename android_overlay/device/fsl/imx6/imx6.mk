1e32556541f4fcc7ea1568d28e196aaef129ba5b
SusanChoi <susan@akaspace.kr>
2017-02-24 13:52:51 +0900

    add naviate bar deleter

--- android_overlay/device/fsl/imx6/imx6.mk
+++ android_overlay/device/fsl/imx6/imx6.mk
@@ -366,7 +366,8 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 # for property
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
-	persist.sys.usb.config=mtp
+	persist.sys.usb.config=mtp \
+	qemu.hw.mainkeys=1 	
 
 PRODUCT_DEFAULT_DEV_CERTIFICATE := \
         device/fsl/common/security/testkey
